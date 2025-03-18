b4099ebcd04adbb2104ac28294547754aad837a7
Roberta Nascimento <167623559+zurannii@users.noreply.github.com>
2025-03-18 23:01:48 +0000

    adcionando de java

---
+++ ContaBancaria.java
@@ -0,0 +1,33 @@
+public class ContaBancaria {
+    private String titular;
+    private String numeroConta;
+    private double saldo;
+
+    public ContaBancaria(){}
+    public ContaBancaria(String titular, String numeroConta){
+        this.titular = titular;
+        this. numeroConta = numeroConta;
+    }
+    public double getSaldo (){
+        public void getSaldo(double saldo){
+            this.saldo = saldo;
+        }
+
+    public void depositar (double valor) throw  Exeption {
+        if (valor <= 0) {
+            throw new Exeption();
+            }
+            saldo = saldo + valor;
+        }
+        public void sacar (double valor) throw Exeption{
+            if (valor > saldo \\ valor <= 0) {
+                throw new Exeption();
+            }
+            saldo = valor; 
+        }
+        public class static void Main (sacar a) {
+            ContaBancaria
+        }
+    }
+}
+
